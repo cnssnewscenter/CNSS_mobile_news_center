58b6e836af4cee541ddad1556b22baaece178b51
Pandada8 <pandada8@gmail.com>
2015-05-27 18:44:31 +0800

    Add gulp for compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,33 @@
-var gulp = require("gulp");
-gulp.task('default', function() {
-      // place code for your default task here
+var gulp = require("gulp"),
+    useref = require('gulp-useref'),
+    gulpif = require("gulp-if"),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    rev = require("gulp-rev"),
+    revReplace = require("gulp-rev-replace")
+    minifyInline = require('gulp-minify-inline');
+
+gulp.task('script', function() {
+    var assets = useref.assets({
+    });
+    return gulp.src("static/index.html")
+        .pipe(assets)
+        .pipe(gulpif("*.js", uglify()))
+        .pipe(gulpif("*.js", minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(revReplace())
+        .pipe(gulp.dest("dist"))
 });
+gulp.task("inline_index", function() {
+    return gulp.src('static/*.html')
+        .pipe(minifyInline())
+        .pipe(gulp.dest("static/"))
+})
+gulp.task("inline_template", function(){
+    return gulp.src("static/templates/*.html")
+        .pipe(minifyInline())
+        .pipe(gulp.dest("static/templates/"))
+})
+gulp.task("inline", ["inline_index", "inline_template"])
+gulp.task("default", ["inline", "script"])
