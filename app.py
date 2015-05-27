7164149e12805da04e7ca646b4d62af7932b8274
Pandada8 <pandada8@gmail.com>
2015-05-27 18:40:53 +0800

    Add dev and production compress

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tornado.web
 from tornado.options import define, options, parse_config_file
 import os
 
+
 def config():
     define('CACHE_TIME', default=5 * 60, help="The cached time for every news, in seconds")
     define('DEBUG', default=True, help="The global debug control")
@@ -19,13 +20,17 @@ def runapp():
         "debug": options.DEBUG,
         "static_path": os.path.join(os.path.dirname(__file__), "static")
     }
-
-    application = tornado.web.Application([
+    route = [
         ("/api/p/(\d*)", views.News),
         ("/api/index", views.Index),
-        ("/comp/(.*)", tornado.web.StaticFileHandler, {'path': "bower_components/"}),
+        None,
         ("/(.*)", views.RedirectStaticFileHandler, {"path": 'static/index.html'})
-        ], **settings)
+    ]
+    if options.DEBUG:
+        route[2] = ("/bower_components/(.*)", tornado.web.StaticFileHandler, {'path': "bower_components/"})
+    else:
+        route[2] = ("/dist/(.*)", tornado.web.StaticFileHandler, {'path': "dist/"})
+    application = tornado.web.Application(route, **settings)
     application.listen(8001)
     tornado.ioloop.IOLoop.instance().start()
 
