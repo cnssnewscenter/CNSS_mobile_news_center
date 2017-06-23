0f57d2110b990e6c4f9bf9a2d909b89c32fadb04
pandada8 <pandada8@gmail.com>
2017-06-23 18:31:48 +0800

    change page title

--- static/js/app.js
+++ static/js/app.js
@@ -73,6 +73,7 @@
     app.run(["$rootScope", 'api', "$route", "$location", "$log", function($rootScope, api, $route, $location, $log){
         api.changeTitle = function(title){
             $rootScope.title = title
+            document.title = title
         }
         api.loading_finish = function(){
             $rootScope.loading = false;
